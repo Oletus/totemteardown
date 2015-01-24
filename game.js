a0e0eb8addb6715f355821f5069353b87e67379a
Olli Etuaho <olli.etuaho@gmail.com>
2015-01-23 21:59:22 -0800

    Add game loop with fixed update FPS

--- game.js
+++ game.js
@@ -1,8 +1,47 @@
+var FPS = 60;
+
 var canvas;
 var ctx;
+var game;
 
-var frame = function() {
-    window.requestAnimationFrame(frame);
+var Game = function() {
+    this.f = 0;
+};
+
+// This runs at fixed 60 FPS
+Game.prototype.update = function() {
+    this.f += 0.5;
+    if (this.f > 100) {
+        this.f = 0;
+    }
+};
+
+Game.prototype.render = function() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, this.f, this.f);
+};
+
+var webFrame = function() {
+    var time = new Date().getTime();
+    var updated = false;
+    var updates = 0;
+    if (time - nextFrameTime > 500) {
+        nextFrameTime = time - 500;
+    }
+    while (time > nextFrameTime) {
+        nextFrameTime += 1000 / FPS;
+        game.update();
+        updates++;
+    }
+    /*if (updates > 1) {
+        console.log('dropped ' + (updates - 1) + ' frames');
+    }*/
+    if (updates > 0) {
+        game.render();
+    }
+    requestAnimationFrame(webFrame);
 };
 
 var initGame = function() {
@@ -12,5 +51,8 @@ var initGame = function() {
     document.body.appendChild(canvas);
     ctx = canvas.getContext('2d');
 
-    frame();
+    game = new Game();
+    
+    nextFrameTime = new Date().getTime() - 1000 / FPS * 0.5;
+    webFrame();
 };
