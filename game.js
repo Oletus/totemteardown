0abe3801fe6c228b20cb0db3783ece59bfbd8cd8
Olli Etuaho <olli.etuaho@gmail.com>
2015-01-23 21:45:33 -0800

    Boilerplate

---
+++ game.js
@@ -0,0 +1,16 @@
+var canvas;
+var ctx;
+
+var frame = function() {
+    window.requestAnimationFrame(frame);
+};
+
+var initGame = function() {
+    canvas = document.createElement('canvas');
+    canvas.width = 800;
+    canvas.height = 600;
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+
+    frame();
+};
