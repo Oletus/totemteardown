af57c0eaffab6b0ea9c72c0e51a999d13708f144
Olli Etuaho <olli.etuaho@gmail.com>
2015-01-25 20:18:58 -0800

    Remove field.js

--- field.js
+++
@@ -1,9 +0,0 @@
-function Field(point, mass) {
-    this.position = point;
-    this.setMass(mass);
-}
-
-Field.prototype.setMass = function(mass) {
-    this.mass = mass || 1500;
-    this.drawColor = mass < 0 ? "#f00" : "#0f0";
-}
